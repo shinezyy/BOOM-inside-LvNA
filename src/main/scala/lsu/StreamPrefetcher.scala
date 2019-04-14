40737e3bcd9b007119572a464836d2f7c73a2b82
Zhou Yaoyang <shinezyy@qq.com>
2019-04-14 13:31:43 +0800

    is to abort current design

--- src/main/scala/lsu/StreamPrefetcher.scala
+++ src/main/scala/lsu/StreamPrefetcher.scala
@@ -6,6 +6,7 @@ import chisel3._
 import chisel3.internal.naming.chiselName
 import chisel3.util._
 import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.tile.HasL1CacheParameters
 import freechips.rocketchip.util.GTimer
 
 trait T2StateConstants {
@@ -71,7 +72,7 @@ class ExpandedSITSignals (implicit p: Parameters) extends BoomBundle()(p)
   val addr = UInt(coreMaxAddrBits.W)
 }
 
-class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
+class StreamPrefetcher (implicit p: Parameters) extends BoomModule ()(p)
   with T2Parameters
   with T2StateConstants
   with PrefetcherConstants
@@ -85,8 +86,6 @@ class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
   })
 
   ////////////////////// s1
-  private val mPC = RegInit(0.U(Addrbits.W))
-
   val data_array = SyncReadMem(SIT_SIZE, new SITEntry())
 
   val mPCEntries = for (i <- 0 until SIT_SIZE) yield {val slot = Module(new MPCEntry()); slot}
@@ -96,8 +95,14 @@ class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
 
   val s1_hit_wire = io.df.valid && mPCEntryPorts.map(_.eq).reduce(_|_) // any match
 
+  val blockMask_1 = (((1L << Addrbits) - 1) ^ ((1 << 6) - 1))
+  val blockMask = (((1L << Addrbits) - 1) ^ ((1 << 6) - 1)).U(Addrbits.W)
+  println(f"block mask: 0x$blockMask_1%x")
+
+  val masked_addr = (io.df.bits.addr & blockMask).asUInt()
+
   dprintf(D_T2_2, s1_hit_wire, "[%d] access hit in mPCEntries with pc0x%x, addr 0x%x\n",
-    GTimer(), io.df.bits.pc, io.df.bits.addr)
+    GTimer(), io.df.bits.pc, masked_addr)
 
   val s1_hit_ptr_w = OHToUInt(mPCEntryPorts.map(_.eq))
   val s1_hit_ptr = RegNext(s1_hit_ptr_w)
@@ -108,11 +113,12 @@ class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
   s1_info.valid := io.df.valid && io.isInLoop
   s1_info.bits.hit := s1_hit_wire
   s1_info.bits.miss := s1_miss_wire
-  s1_info.bits.addr := io.df.bits.addr
+  s1_info.bits.addr := masked_addr
   s1_info.bits.pc := io.df.bits.pc
 
   //              |     s1      |                 s2                |
-  val s1_kill_s2 = s1_miss_wire && s1_info.valid && s1_info.bits.miss
+  val s1_kill_s2 = s1_miss_wire && s1_info.valid && s1_info.bits.miss &&
+    s1_info.bits.addr === masked_addr
 
   dprintf(D_T2, io.df.valid && io.isInLoop,
     "[%d] s1_miss wire: %d, s1_hit wire: %d, s1_pc wire: 0x%x\n",
@@ -168,14 +174,12 @@ class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
 
   s3_wb_entry_w.delta := new_delta
 
-  when (s2_info.valid && s2_info.bits.hit) {
-    // keep most states by default
+  when (s2_info.valid) {
     s3_wb_entry_w := s2_read_sit_entry
-    s3_wb_entry_w.delta := new_delta
     s3_wb_entry_w.lastAddr := s2_info.bits.addr
-
-
+    val changed = WireInit(false.B)
     when(io.s2_miss) {
+      changed := true.B
       dprintf(D_T2_1, s2_info.valid && s2_info.bits.hit,
         "[%d] s2 processing pc0x%x addr: 0x%x!\n",
         GTimer(), s2_info.bits.pc, s2_info.bits.addr)
@@ -189,52 +193,61 @@ class StreamPrefetcher (implicit p: Parameters) extends BoomModule()(p)
           "set to OB\n",
           GTimer(), s2_info.bits.pc, s2_info.bits.addr)
         s3_wb_entry_w.state := TS_OB
+        s3_wb_entry_w.delta := new_delta
       }
 
       dprintf(D_T2_2, "[%d] l1d miss by pc0x%x addr[0x%x], delta old: %d, new:%d\n",
         GTimer(), s2_info.bits.pc, s2_info.bits.addr, s2_read_sit_entry.delta, new_delta)
 
-      val changed = WireInit(false.B)
-
-      when(new_delta === s2_read_sit_entry.delta) {
-        changed := true.B
-        dprintf(D_T2_1, "[%d] sit ptr: %d\n", GTimer(), s2_info.bits.sit_ptr)
-
-        when(s2_read_sit_entry.state === TS_OB && new_delta =/= 0.U) {
-          dprintf(D_T2_1, "[%d] set to striding\n", GTimer())
-          s3_wb_entry_w.state := TS_STRD
-          s3_wb_entry_w.counter := 1.U(StrideCounterWidth.W)
-
-        }.elsewhen(s2_read_sit_entry.state === TS_STRD &&
-          s2_read_sit_entry.counter =/= ((1 << StrideCounterWidth) - 1).U(StrideCounterWidth.W)) { // saturating counter
-
-          dprintf(D_T2_1, "[%d] inc striding\n", GTimer())
-          s3_wb_entry_w.state := TS_STRD
-          s3_wb_entry_w.counter := (s2_read_sit_entry.counter + 1.U)(StrideCounterWidth-1, 0)
-
-          for (i <- 0 until SIT_SIZE) {
-            dprintf(D_T2_1, "SIT[%d]: 0x%x\n",
-              i.U, data_array.read(i.U).lastAddr)
-          }
-        }
-      }.elsewhen(s2_read_sit_entry.state === TS_STRD) { // not equal
-        changed := true.B
-        s3_wb_entry_w.state := TS_OB
-        s3_wb_entry_w.counter := 0.U(StrideCounterWidth-1, 0)
-
-        dprintf(D_T2_1, "[%d] Set striding back to OB\n", GTimer())
-        dprintf(D_T2_1, "[%d] sit ptr: %d\n", GTimer(), s2_info.bits.sit_ptr)
-      }
-      when (changed) {
-        for (i <- 0 until SIT_SIZE) {
-          val e = data_array.read(i.U)
-          dprintf(D_T2_1, "SIT[%d], pc: 0x%x, addr: 0x%x, state: %d, delta: %d\n",
-            i.U, mPCEntryPorts(i).cell, e.lastAddr, e.state, e.delta)
-        }
-      }
     }.otherwise {
       dprintf(D_T2_2, "[%d] pc0x%x addr: 0x%x hit in SIT but not miss!\n",
         GTimer(), s2_info.bits.pc, s2_info.bits.addr)
+    }
+
+
+    when (s2_info.bits.hit) {
+      // keep most states by default
+      s3_wb_entry_w.delta := s2_read_sit_entry.delta
+
+      when(new_delta =/= 0.U) {
+        s3_wb_entry_w.delta := new_delta
+        when(new_delta === s2_read_sit_entry.delta) {
+          changed := true.B
+          dprintf(D_T2_1, "[%d] sit ptr: %d\n", GTimer(), s2_info.bits.sit_ptr)
+
+          when(s2_read_sit_entry.state === TS_OB && new_delta =/= 0.U) {
+            dprintf(D_T2_1, "[%d] set to striding\n", GTimer())
+            s3_wb_entry_w.state := TS_STRD
+            s3_wb_entry_w.counter := 1.U(StrideCounterWidth.W)
+
+          }.elsewhen(s2_read_sit_entry.state === TS_STRD &&
+            s2_read_sit_entry.counter =/= ((1 << StrideCounterWidth) - 1).U(StrideCounterWidth.W)) { // saturating counter
+
+            dprintf(D_T2_1, "[%d] inc striding\n", GTimer())
+            s3_wb_entry_w.state := TS_STRD
+            s3_wb_entry_w.counter := (s2_read_sit_entry.counter + 1.U) (StrideCounterWidth - 1, 0)
+
+            for (i <- 0 until SIT_SIZE) {
+              dprintf(D_T2_1, "SIT[%d]: 0x%x\n",
+                i.U, data_array.read(i.U).lastAddr)
+            }
+          }
+        }.elsewhen(s2_read_sit_entry.state === TS_STRD) { // not equal
+          changed := true.B
+          s3_wb_entry_w.state := TS_OB
+          s3_wb_entry_w.counter := 0.U(StrideCounterWidth - 1, 0)
+
+          dprintf(D_T2_1, "[%d] Set striding back to OB\n", GTimer())
+          dprintf(D_T2_1, "[%d] sit ptr: %d\n", GTimer(), s2_info.bits.sit_ptr)
+        }
+      }
+    }
+    when (changed) {
+      for (i <- 0 until SIT_SIZE) {
+        val e = data_array.read(i.U)
+        dprintf(D_T2_1, "SIT[%d], pc: 0x%x, addr: 0x%x, state: %d, delta: %d\n",
+          i.U, mPCEntryPorts(i).cell, e.lastAddr, e.state, e.delta)
+      }
     }
   }
   val pref_addr = s2_info.bits.addr + s3_wb_entry_w.delta
